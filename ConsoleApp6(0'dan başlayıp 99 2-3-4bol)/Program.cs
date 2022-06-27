be025ad724936c56947bd1bbcb427d231b3b5e6d
OsmanGurbuz <107862900+OsmanGurbuz@users.noreply.github.com>
2022-06-27 16:34:31 +0300

    0'dan başlayıp 99'a kadar olan sayıların 2-3-4 bölünebilenlerini ekrana yazdıralım
    0'dan başlayıp 99'a kadar olan sayıların 2-3-4 bölünebilenlerini ekrana yazdıralım

---
+++ ConsoleApp6(0'dan başlayıp 99 2-3-4bol)/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi;
+            for (sayi = 0; sayi <= 99; sayi++)
+            {
+
+
+                if ((sayi % 2 == 0) && (sayi % 4 == 0))
+                {
+                    Console.Write(sayi + " ");
+                    Console.WriteLine("2 ve 4 e bölünebilir");
+                }
+                else if ((sayi % 2 == 0))
+                {
+                    Console.Write(sayi + " ");
+                    Console.WriteLine("2 ye bölünebilir");
+
+                }
+                else if (sayi % 3 == 0)
+                {
+                    Console.Write(sayi + " ");
+                    Console.WriteLine("3 e bölünebilir");
+
+                }
+
+            }
+            Console.ReadLine();
+        }
+
+    }
+
+}
+    
+
